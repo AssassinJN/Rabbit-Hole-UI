e20671b027deff74fd6374d36ea5d4267fdc2898
Andrew J Nuttall <contact@andrewjnuttall.com>
2023-04-11 09:04:29 -0400

    add ps update script

---
+++ RabbitHoleUI/updateRabbitHole.ps1
@@ -0,0 +1,9 @@
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/rabbitHoleUI.js -OutFile .\rabbitHoleUI.js
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/rabbitHoleUI.css -OutFile .\rabbitHoleUI.css
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/index.html -OutFile .\index.html
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/favicon.ico -OutFile .\favicon.ico
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/css/stylez.css -Force -OutFile .\css\stylez.css
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/js/localData.js -OutFile .\js\localData.js
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/js/utils.js -OutFile .\js\utils.js
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/images/down-arrow.svg -OutFile .\images\down-arrow.svg
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/AssassinJN/Rabbit-Hole-UI/master/RabbitHoleUI/images/more.svg -OutFile .\images\more.svg
